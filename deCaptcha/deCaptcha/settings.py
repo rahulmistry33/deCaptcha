5024f7e306a2b5a5ab2ece7ddc6ce976c519062d
girishgr8 <girishthatte35@gmail.com>
2021-05-22 21:04:21 +0530

    Added STATIC_ROOT Url

--- deCaptcha/deCaptcha/settings.py
+++ deCaptcha/deCaptcha/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'deCaptcha/static')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'deCaptcha/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'deCaptcha/static')
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'deCaptcha/media')
